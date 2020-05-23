5c15dbb47f556a416defeb17e7420cfd737fa4f0
Baranidharan S <sbaranidharan@athenahealth.com>
2020-05-24 04:12:16 +0530

    Update caesar-cipher-1.hs

--- algorithms/strings/caesar-cipher-1.hs
+++ algorithms/strings/caesar-cipher-1.hs
@@ -6,8 +6,8 @@ encrypt ( _ : str : [k']) = map (encryptChar k) str
 
 encryptChar :: Int -> Char -> Char
 encryptChar k c
-    | isLower c = chr $ (+97) $ ((ord c) - (ord 'a') + k) `mod` 26
-    | isUpper c = chr $ (+65) $ ((ord c) - (ord 'A') + k) `mod` 26
+    | isLower c = chr $ (+ord 'a') $ ((ord c) - (ord 'a') + k) `mod` 26
+    | isUpper c = chr $ (+ord 'A') $ ((ord c) - (ord 'A') + k) `mod` 26
     | otherwise = c
 
 main = interact $ encrypt . words
